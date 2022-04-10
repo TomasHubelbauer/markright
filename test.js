a7e4f26d487c91b34d74965ab243429709ea2629
Tomas Hubelbauer <tomas@hubelbauer.net>
2022-04-10 19:28:38 +0200

    Remove void async wrappers and rely on TLA
    Recent Node versions, like the one running in GitHub Actions, support top-level await now.

--- test.js
+++ test.js
@@ -2,45 +2,44 @@ import fs from 'fs';
 import path from 'path';
 import MarkRight from './MarkRight.js';
 
-// Use by running `npm test` or `npm test ${regex}` to filter only given tests
-void async function () {
-  const filePath = import.meta.url.slice('file:///'.length);
-  const directoryPath = path.dirname(filePath);
+/* Use by running `npm test` or `npm test ${regex}` to filter only given tests */
 
-  // Run JavaScript tests
-  for (const fileName of await fs.promises.readdir(directoryPath)) {
-    if (!fileName.endsWith('.test.js')) {
-      continue;
-    }
+const filePath = import.meta.url.slice('file:///'.length);
+const directoryPath = path.dirname(filePath);
 
-    if (process.argv[2] && !fileName.match(process.argv[2])) {
-      continue;
-    }
-
-    try {
-      const { default: test } = await import('file://' + path.join(directoryPath, fileName));
-      await test();
-    }
-    catch (error) {
-      console.log(fileName, error);
-    }
+// Run JavaScript tests
+for (const fileName of await fs.promises.readdir(directoryPath)) {
+  if (!fileName.endsWith('.test.js')) {
+    continue;
   }
 
-  // Skip MarkDown tests when filtering for a given JavaScript test
-  if (process.argv[2]) {
-    return;
+  if (process.argv[2] && !fileName.match(process.argv[2])) {
+    continue;
   }
 
-  // Run MarkDown tests
-  for (const directoryName of await fs.promises.readdir(path.join(directoryPath, 'example'))) {
-    process.chdir(path.join(directoryPath, 'example', directoryName));
-    const markRight = new MarkRight(path.join(directoryPath, 'example', directoryName, 'readme.md'));
-    try {
-      // TODO: Check this matches the expected snapshot by checking no files Git-changed
-      await markRight.run();
-    }
-    catch (error) {
-      console.log(directoryName, error);
-    }
+  try {
+    const { default: test } = await import('file://' + path.join(directoryPath, fileName));
+    await test();
   }
-}()
+  catch (error) {
+    console.log(fileName, error);
+  }
+}
+
+// Skip MarkDown tests when filtering for a given JavaScript test
+if (process.argv[2]) {
+  process.exit(0);
+}
+
+// Run MarkDown tests
+for (const directoryName of await fs.promises.readdir(path.join(directoryPath, 'example'))) {
+  process.chdir(path.join(directoryPath, 'example', directoryName));
+  const markRight = new MarkRight(path.join(directoryPath, 'example', directoryName, 'readme.md'));
+  try {
+    // TODO: Check this matches the expected snapshot by checking no files Git-changed
+    await markRight.run();
+  }
+  catch (error) {
+    console.log(directoryName, error);
+  }
+}
